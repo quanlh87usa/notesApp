7b03f76635e58a308223880e74e7d63c2b2ce288
quanlh87usa <130505800+quanlh87usa@users.noreply.github.com>
2025-03-18 14:41:44 -0700

    Update axiosinstance.js

--- frontend/src/utils/axiosinstance.js
+++ frontend/src/utils/axiosinstance.js
@@ -2,11 +2,11 @@ import axios from "axios"
 import { BASE_URL } from "./constants"
 
 const axiosInstance = axios.create({
-    baseURL: "https://notesapp-server-lake.vercel.app",
+    baseURL: "https://notesapp-server-lake.vercel.app:8000",
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "https://notesapp-server-lake.vercel.app",
+        "Access-Control-Allow-Origin": "https://notesapp-server-lake.vercel.app:8000",
         "Access-Control-Allow-Credentials": true,
     }
 })
